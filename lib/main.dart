0fc7da239f4a05c9b999e276ab0f09cc9b53b2fd
Christophar Antony <bosc.dev@gmail.com>
2023-05-20 00:12:34 +0530

    first_form_create

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:counter_application/glassmorphism.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,49 +10,140 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      theme: ThemeData(),
+      home: const LoginPage(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  int counter = 0;
+class _LoginPageState extends State<LoginPage> {
+  bool psdShow = false;
+  bool isSubmit = false;
+  Map user = {'username': '', 'password': ''};
+  final _usernameController = TextEditingController();
+  final _psdController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 248, 60, 73),
+      backgroundColor: const Color.fromARGB(255, 255, 110, 84),
       body: SafeArea(
-        child: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              setState(() {
-                counter++;
-              });
-            },
-            onLongPress: () {
-              setState(() {
-                counter = 0;
-              });
-            },
-            child: Text("$counter"),
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            counter++;
-          });
-        },
-        child: const Icon(Icons.add),
+        child: Padding(
+            padding: const EdgeInsets.all(40),
+            child: Column(
+              children: [
+                TextField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Username",
+                      hintText: "Enter your username"),
+                  style: const TextStyle(
+                      letterSpacing: 5,
+                      fontFamily: "Roboto",
+                      color: Color.fromARGB(255, 112, 1, 1)),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  controller: _psdController,
+                  obscureText: !psdShow,
+                  decoration: InputDecoration(
+                      border: const OutlineInputBorder(),
+                      labelText: "Password",
+                      hintText: "Enter your password",
+                      suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              psdShow = !psdShow;
+                            });
+                          },
+                          icon: psdShow
+                              ? const Icon(Icons.visibility)
+                              : const Icon(Icons.visibility_off))),
+                  style: const TextStyle(
+                      letterSpacing: 5,
+                      fontFamily: "Roboto",
+                      color: Color.fromARGB(255, 112, 1, 1)),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        isSubmit = true;
+                      });
+                    },
+                    style: const ButtonStyle(),
+                    child: const Text(
+                      "Submit",
+                      style: TextStyle(
+                        letterSpacing: 5,
+                        fontFamily: "Roboto",
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        isSubmit = false;
+                        psdShow = false;
+                        _usernameController.clear();
+                        _psdController.clear();
+                      });
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          const Color.fromARGB(255, 255, 49, 49)),
+                    ),
+                    child: const Text(
+                      "Reset",
+                      style: TextStyle(
+                        letterSpacing: 5,
+                        fontFamily: "Roboto",
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                isSubmit
+                    ? GlassMorphism(
+                        start: 0.9,
+                        end: 0.6,
+                        child: Column(children: [
+                          SizedBox(
+                            width: double.infinity,
+                            height: 150,
+                            child: Center(
+                              child: Text(
+                                  "Welcome to our Family,\nName: ${_usernameController.text}\n${psdShow ? "Password: ${_psdController.text}" : "Password: ********"}"),
+                            ),
+                          ),
+                        ]),
+                      )
+                    : Container()
+              ],
+            )),
       ),
     );
   }
